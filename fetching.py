ffc240c555e6af373eca87eb4d40be5cd9c32cf6
Thomas Legrand <legrand.thomas5@hotmail.fr>
2020-01-22 17:35:41 +0100

    switch to station_id

--- fetching.py
+++ fetching.py
@@ -1,9 +1,9 @@
-import requests
 import time
-import numpy as np
-import pandas as pd
 from datetime import datetime
 from pathlib import Path
+
+import pandas as pd
+import requests
 
 STATION_STATUS_URL = "https://velib-metropole-opendata.smoove.pro/opendata/Velib_Metropole/station_status.json"
 DATA_ROOT = "data/stations_status"
@@ -15,22 +15,22 @@ while True:
         stations = req.json().get('data').get('stations')
 
         for station in stations:
-            STATION_PATH = f'{DATA_ROOT}/{station.get("stationCode")}.csv'
+            STATION_PATH = f'{DATA_ROOT}/{station.get("station_id")}.csv'
             mechanical = station.get('num_bikes_available_types')[0].get('mechanical')
             ebike = station.get('num_bikes_available_types')[1].get('ebike')
             del station['num_bikes_available_types']
             if Path(STATION_PATH).exists():
                 old_state = pd.read_csv(STATION_PATH)
                 n = len(old_state)
-                if int(station.get('last_reported')) > int(old_state.loc[n-1, 'last_reported']):
-                    print(f'status updated for station {station.get("stationCode")}')
-                    new_state = pd.DataFrame(station, index=[station.get("stationCode")])
+                if int(station.get('last_reported')) > int(old_state.loc[n - 1, 'last_reported']):
+                    print(f'status updated for station {station.get("station_id")}')
+                    new_state = pd.DataFrame(station, index=[station.get("station_id")])
                     new_state['mechanical'] = mechanical
                     new_state['ebike'] = ebike
                     dataframe = pd.concat([old_state, new_state])
                     dataframe.to_csv(STATION_PATH, index=None)
             else:
-                dataframe = pd.DataFrame(station, index=[station.get("stationCode")])
+                dataframe = pd.DataFrame(station, index=[station.get("station_id")])
                 dataframe['mechanical'] = mechanical
                 dataframe['ebike'] = ebike
                 dataframe.to_csv(STATION_PATH, index=None)
--- modelling.py
+++ modelling.py
@@ -1,17 +1,18 @@
-from fbprophet import Prophet
-from fbprophet.plot import plot_plotly
 import pandas as pd
 import plotly.offline as py
+from fbprophet import Prophet
+from fbprophet.plot import plot_plotly
 
-df = pd.read_csv('./data/stations_status/6497.csv')
-df['ds'] = pd.to_datetime(df['last_reported'],unit='s')
+df = pd.read_csv('./data/stations_status/6512.csv')
+df['ds'] = pd.to_datetime(df['last_reported'], unit='s')
 df['y'] = df['mechanical']
 
 m = Prophet()
 m.fit(df)
 
-future = m.make_future_dataframe(periods=1)
+future = m.make_future_dataframe(periods=6, freq='H')
 forecast = m.predict(future)
+print(forecast)
 
-fig = plot_plotly(m, forecast)  # This returns a plotly Figure
-py.plot(fig)
+# fig = plot_plotly(m, forecast)
+# py.plot(fig)
