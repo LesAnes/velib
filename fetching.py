299026392bfd83c7368ae1cd2441fde498789c50
Aymeric Dominique <aymeric.dominique@gmail.com>
2020-02-08 16:04:09 +0100

    update mapping

--- fetching.py
+++ fetching.py
@@ -11,20 +11,19 @@ def main():
     stations = fetch_velib_api()
     for station in stations:
         # Api mapping
-        mechanical = station.get('num_bikes_available_types')[0].get('mechanical')
-        ebike = station.get('num_bikes_available_types')[1].get('ebike')
+        station['mechanical'] = station.get('num_bikes_available_types')[0].get('mechanical')
+        station['ebike'] = station.get('num_bikes_available_types')[1].get('ebike')
+        station['station_code'] = station.get('stationCode')
         del station['num_bikes_available_types']
         del station['numBikesAvailable']
         del station['numDocksAvailable']
         
-        status_by_station_id = col.find({"station_id": station.get("station_id")})
-        status_by_station_id_sorted = status_by_station_id.sort("last_reported", -1)
+        status_by_station_id = col.find({'station_id': station.get('station_id')})
+        status_by_station_id_sorted = status_by_station_id.sort('last_reported', -1)
         last_reported = status_by_station_id_sorted[0].get('last_reported') if status_by_station_id_sorted.count() > 0 else None
         if last_reported and int(station.get('last_reported')) > int(last_reported):
-            print(f'status updated for station {station.get("station_id")}')
-            station['mechanical'] = mechanical
-            station['ebike'] = ebike
+            print(f'status updated for station {station.get('station_id')}')
             col.insert_one(station)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
