9a903a5bb87c102a133fc039c6bb60791b39751f
Thomas Legrand <legrand.thomas5@hotmail.fr>
2020-03-25 16:51:17 +0100

    add max 100 score

--- scoring.py
+++ scoring.py
@@ -6,5 +6,5 @@ def score_station(station, departure=True):
         score = 10000 * station["num_bikes_available"] / (station["capacity"] * station["distance"])
     else:
         score = 10000 * station["num_docks_available"] / (station["capacity"] * station["distance"])
-    station["score"] = math.floor(score)
+    station["score"] = min(math.floor(score), 100)
     return station
