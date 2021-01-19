292e4dd437eba6e92e4ac8488831eb5e4cb3c9ac
Thomas Legrand <legrand.thomas5@hotmail.fr>
2021-01-19 22:49:36 +0100

    fix delete old status

--- db.py
+++ db.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 
 from models import LatLngBoundsLiteral, Coordinate, Feedback, FeedbackType
 
-dotenv_path = join(dirname(__file__), '.env')
+dotenv_path = join(dirname(__file__), ".env")
 load_dotenv(dotenv_path)
 
 myclient = pymongo.MongoClient(
@@ -43,10 +43,9 @@ def get_station_status(station_id) -> list:
 
 def remove_old_status() -> None:
     col = get_stations_status_collection()
-    one_months_ago = (datetime.now() - timedelta(days=30)).strftime("%s")
-    col.delete_many({"last_reported": {"$lt": one_months_ago}})
-    print(
-        f'removed status before {datetime.fromtimestamp(float(one_months_ago))}')
+    one_week_ago = (datetime.now() - timedelta(days=7)).strftime("%s")
+    col.delete_many({"last_reported": {"$lt": int(one_week_ago)}})
+    print(f"removed status before {datetime.fromtimestamp(float(one_week_ago))}")
 
 
 def get_last_station_status(station_id) -> list:
@@ -57,8 +56,22 @@ def get_last_station_status(station_id) -> list:
 def get_last_stations_status(station_ids, departure=True) -> list:
     col = get_stations_last_state_collection()
     if departure:
-        return list(col.find({"station_id": {"$in": station_ids}, "is_installed": 1, "is_renting": 1}, {"_id": 0}))
-    return list(col.find({"station_id": {"$in": station_ids}, "is_installed": 1, "is_returning": 1}, {"_id": 0}))
+        return list(
+            col.find(
+                {
+                    "station_id": {"$in": station_ids},
+                    "is_installed": 1,
+                    "is_renting": 1,
+                },
+                {"_id": 0},
+            )
+        )
+    return list(
+        col.find(
+            {"station_id": {"$in": station_ids}, "is_installed": 1, "is_returning": 1},
+            {"_id": 0},
+        )
+    )
 
 
 def get_station_information(station_id) -> list:
@@ -66,57 +79,71 @@ def get_station_information(station_id) -> list:
     return list(col.find({"station_id": station_id}, {"_id": 0}))[0]
 
 
-def get_stations_information_in_polygon(lat_lng_bounds_literal: LatLngBoundsLiteral,
-                                        current_position: Coordinate = None) -> list:
+def get_stations_information_in_polygon(
+    lat_lng_bounds_literal: LatLngBoundsLiteral, current_position: Coordinate = None
+) -> list:
     col = get_station_information_collection()
-    return list(col.find({
-        "loc": {
-            "$geoWithin": {
-                "$box": [
-                    [lat_lng_bounds_literal.west, lat_lng_bounds_literal.south],
-                    [lat_lng_bounds_literal.east, lat_lng_bounds_literal.north]
-                ]
-            }
-        }
-    }, {"_id": 0}))
+    return list(
+        col.find(
+            {
+                "loc": {
+                    "$geoWithin": {
+                        "$box": [
+                            [lat_lng_bounds_literal.west, lat_lng_bounds_literal.south],
+                            [lat_lng_bounds_literal.east, lat_lng_bounds_literal.north],
+                        ]
+                    }
+                }
+            },
+            {"_id": 0},
+        )
+    )
 
 
-def get_station_information_with_distance(station_id: int, latitude: float, longitude: float) -> list:
+def get_station_information_with_distance(
+    station_id: int, latitude: float, longitude: float
+) -> list:
     col = get_station_information_collection()
-    return list(col.aggregate([
-        {
-            "$geoNear": {
-                "near": {"type": "Point", "coordinates": [longitude, latitude]},
-                "key": "loc",
-                "distanceField": "distance",
-            }
-        },
-        {"$match": {"station_id": station_id}},
-    ]))
+    return list(
+        col.aggregate(
+            [
+                {
+                    "$geoNear": {
+                        "near": {"type": "Point", "coordinates": [longitude, latitude]},
+                        "key": "loc",
+                        "distanceField": "distance",
+                    }
+                },
+                {"$match": {"station_id": station_id}},
+            ]
+        )
+    )
 
 
 def get_closest_stations_information(latitude: float, longitude: float) -> list:
     col = get_station_information_collection()
-    return list(col.aggregate([
-        {
-            "$geoNear": {
-                "near": {"type": "Point", "coordinates": [longitude, latitude]},
-                "key": "loc",
-                "distanceField": "distance",
-                "maxDistance": 1000
-            }
-        }, {"$limit": 15}
-    ]))
+    return list(
+        col.aggregate(
+            [
+                {
+                    "$geoNear": {
+                        "near": {"type": "Point", "coordinates": [longitude, latitude]},
+                        "key": "loc",
+                        "distanceField": "distance",
+                        "maxDistance": 1000,
+                    }
+                },
+                {"$limit": 15},
+            ]
+        )
+    )
 
 
 def update_station_last_state(station):
     col = get_stations_last_state_collection()
     if station.get("_id"):
-        station.pop('_id')
-    col.update_one(
-        {"station_id": station.get('station_id')},
-        {"$set": station}
-    )
+        station.pop("_id")
+    col.update_one({"station_id": station.get("station_id")}, {"$set": station})
 
 
 def submit_feedback(feedback) -> None:
@@ -144,31 +171,53 @@ def handle_not_number_feedback(feedback_value: str, field_value: int) -> int:
 
 def apply_feedback(feedback: Feedback):
     s_status = get_last_station_status(feedback.stationId)
-    station_total = int(s_status["num_bikes_available"]) + \
-        int(s_status["num_docks_available"])
+    station_total = int(s_status["num_bikes_available"]) + int(
+        s_status["num_docks_available"]
+    )
     if feedback.type == FeedbackType.confirmed:
-        s_status["mechanical"] = feedback.mechanical \
-            if is_number_feedback(feedback.mechanical) \
+        s_status["mechanical"] = (
+            feedback.mechanical
+            if is_number_feedback(feedback.mechanical)
             else handle_not_number_feedback(feedback.mechanical, s_status["mechanical"])
-        s_status["ebike"] = feedback.ebike \
-            if is_number_feedback(feedback.ebike) \
+        )
+        s_status["ebike"] = (
+            feedback.ebike
+            if is_number_feedback(feedback.ebike)
             else handle_not_number_feedback(feedback.ebike, s_status["ebike"])
-        s_status["num_docks_available"] = feedback.dock \
-            if is_number_feedback(feedback.dock) \
-            else handle_not_number_feedback(feedback.dock, s_status["num_docks_available"])
+        )
+        s_status["num_docks_available"] = (
+            feedback.dock
+            if is_number_feedback(feedback.dock)
+            else handle_not_number_feedback(
+                feedback.dock, s_status["num_docks_available"]
+            )
+        )
     else:
-        s_status["mechanical"] = min(station_total,
-                                     max(0, int(s_status["mechanical"]) - int(feedback.mechanical))) \
-            if is_number_feedback(feedback.mechanical) \
+        s_status["mechanical"] = (
+            min(
+                station_total,
+                max(0, int(s_status["mechanical"]) - int(feedback.mechanical)),
+            )
+            if is_number_feedback(feedback.mechanical)
             else handle_not_number_feedback(feedback.mechanical, s_status["mechanical"])
-        s_status["ebike"] = min(station_total, max(0, int(s_status["ebike"]) - int(feedback.ebike))) \
-            if is_number_feedback(feedback.ebike) \
+        )
+        s_status["ebike"] = (
+            min(station_total, max(0, int(s_status["ebike"]) - int(feedback.ebike)))
+            if is_number_feedback(feedback.ebike)
             else handle_not_number_feedback(feedback.ebike, s_status["ebike"])
-        s_status["num_docks_available"] = min(station_total,
-                                              max(0, int(s_status["num_docks_available"]) - int(feedback.dock))) \
-            if is_number_feedback(feedback.dock) \
-            else handle_not_number_feedback(feedback.dock, s_status["num_docks_available"])
-    s_status["num_bikes_available"] = int(
-        s_status["mechanical"]) + int(s_status["ebike"])
+        )
+        s_status["num_docks_available"] = (
+            min(
+                station_total,
+                max(0, int(s_status["num_docks_available"]) - int(feedback.dock)),
+            )
+            if is_number_feedback(feedback.dock)
+            else handle_not_number_feedback(
+                feedback.dock, s_status["num_docks_available"]
+            )
+        )
+    s_status["num_bikes_available"] = int(s_status["mechanical"]) + int(
+        s_status["ebike"]
+    )
 
     update_station_last_state(s_status)
--- fetching.py
+++ fetching.py
@@ -1,7 +1,11 @@
 from datetime import datetime
 
-from db import get_stations_status_collection, get_stations_last_state_collection, update_station_last_state, \
-    remove_old_status
+from db import (
+    get_stations_status_collection,
+    get_stations_last_state_collection,
+    update_station_last_state,
+    remove_old_status,
+)
 from velib_api import fetch_velib_api
 
 stations_status_col = get_stations_status_collection()
@@ -14,22 +18,32 @@ def main():
     remove_old_status()
     for station in stations:
         # Api mapping
-        station['mechanical'] = station.get('num_bikes_available_types')[0].get('mechanical')
-        station['ebike'] = station.get('num_bikes_available_types')[1].get('ebike')
-        station['station_code'] = station.get('stationCode')
-        del station['num_bikes_available_types']
-        del station['numBikesAvailable']
-        del station['numDocksAvailable']
+        station["mechanical"] = station.get("num_bikes_available_types")[0].get(
+            "mechanical"
+        )
+        station["ebike"] = station.get("num_bikes_available_types")[1].get("ebike")
+        station["station_code"] = station.get("stationCode")
+        del station["num_bikes_available_types"]
+        del station["numBikesAvailable"]
+        del station["numDocksAvailable"]
 
-        status_by_station_id = stations_status_col.find({'station_id': station.get('station_id')})
-        status_by_station_id_sorted = status_by_station_id.sort('last_reported', -1)
-        last_reported = status_by_station_id_sorted[0].get(
-            'last_reported') if status_by_station_id_sorted.count() > 0 else None
-        if last_reported and int(station.get('last_reported')) > int(last_reported):
+        status_by_station_id = stations_status_col.find(
+            {"station_id": station.get("station_id")}
+        )
+        status_by_station_id_sorted = status_by_station_id.sort("last_reported", -1)
+        last_reported = (
+            status_by_station_id_sorted[0].get("last_reported")
+            if status_by_station_id_sorted.count() > 0
+            else None
+        )
+
+        if last_reported is None or int(station.get("last_reported")) > int(
+            last_reported
+        ):
             print(f'status updated for station {station.get("station_id")}')
             stations_status_col.insert_one(station)
             update_station_last_state(station)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
